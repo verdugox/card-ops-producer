885ca1e133cae228cb19f789f85890956bf821e3
verdugox <luisandresverdugox@gmail.com>
2025-09-08 10:32:34 -0500

    feat:Subiendo librerias y proyecto inicial de card-ops-producer con todas sus dependencias listas

---
+++ card-ops-producer/src/test/java/com/bank/card_ops_producer/CardOpsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.card_ops_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardOpsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
